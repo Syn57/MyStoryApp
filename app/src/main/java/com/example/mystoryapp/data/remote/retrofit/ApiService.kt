fb521aa79be71a7638677a2ff934190d43fc9cbc
Syn57 <luthfi.n57@gmail.com>
2022-04-13 13:45:24 +0700

    Adding item row story and Api Service for get stories

--- app/src/main/java/com/example/mystoryapp/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/example/mystoryapp/data/remote/retrofit/ApiService.kt
@@ -2,16 +2,10 @@ package com.example.mystoryapp.data.remote.retrofit
 
 import com.example.mystoryapp.data.remote.response.LoginResponse
 import com.example.mystoryapp.data.remote.response.RegistResponse
+import com.example.mystoryapp.data.remote.response.StoryResponse
 import retrofit2.Call
 import retrofit2.http.*
 
-//interface ApiService {
-//    @POST("/login")
-//    fun getLogin(
-//        @Body("email") email: String,
-//        @Body("password") password: String
-//    ): Call<LoginResponse>
-//}
 interface ApiService {
 
     @FormUrlEncoded
@@ -28,4 +22,11 @@ interface ApiService {
         @Field("email") email: String,
         @Field("password") password: String
     ): Call<RegistResponse>
+
+    @GET("stories")
+    fun getStories(
+        @Query("size") size: Int,
+        @Query("page") page: Int,
+        @Header("Authorization") token: String
+    ): Call<StoryResponse>
 }
